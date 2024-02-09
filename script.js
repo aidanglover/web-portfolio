9f354834908ed290c17ee9e8703d6b8be5fd2c6b
salty2kk <88125340+salty2kk@users.noreply.github.com>
2024-02-09 19:41:30 +1100

    changes

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ document.addEventListener('scroll', function () {
     progress.style.width = scrollPercent + '%';
 });
 
-
 // image fade with hover
 // Function to create an image container
 function createImageContainer(src, alt, title, description) {
@@ -44,30 +43,30 @@ const container = document.querySelector('.container');
 // Create and append multiple image containers
 container.appendChild(createImageContainer('images/abstract.jpg', 'Image 1', 'Abstract', 'Super cool project with some really important stuff. Great to be a part of this.'));
 container.appendChild(createImageContainer('images/orange.png', 'Image 2', 'Orange', 'Check out this orange image. Pretty neat right? I thought so too.'));
-// Add more images as needed
+// container.appendChild(createImageContainer('images/eye.png', 'Image 3', 'Eye', 'Check out this orange image. Pretty neat right? I thought so too.'));
 
+
+// Add more images as needed
 const firstImageContainer = document.querySelectorAll('.image-container')[0];
 const secondImageContainer = document.querySelectorAll('.image-container')[1];
+// const thirdImageContainer = document.querySelectorAll('.image-container')[2];
 
 // Ensure both containers are positioned relatively
-firstImageContainer.style.position = 'relative';
-secondImageContainer.style.position = 'relative';
+firstImageContainer.style.position = 'absolute';
+secondImageContainer.style.position = 'absolute';
 
 firstImageContainer.style.top = '200px'; // Adjust the value as needed
-secondImageContainer.style.top = '300px'; // Adjust the value as needed
+secondImageContainer.style.top = '400px'; // Adjust the value as needed
 
 // Adjust the left position of the second image container
-secondImageContainer.style.left = '-300px'; // Adjust the value as needed
+secondImageContainer.style.left = '600px'; // Adjust the value as needed
+secondImageContainer.style.transform = 'scale(0.9)';
 
 // Optionally, adjust the z-index to ensure proper layering
 firstImageContainer.style.zIndex = '2';
 secondImageContainer.style.zIndex = '1';
 
-
-
-
 // z value controller for hover
-
 const initialZIndexFirst = getComputedStyle(firstImageContainer).zIndex;
 const initialZIndexSecond = getComputedStyle(secondImageContainer).zIndex;
 
@@ -77,7 +76,6 @@ firstImageContainer.addEventListener('mouseenter', function() {
 firstImageContainer.addEventListener('mouseleave', function() {
     firstImageContainer.style.zIndex = initialZIndexFirst;
 });
-
 secondImageContainer.addEventListener('mouseenter', function() {
     secondImageContainer.style.zIndex = '2';
 });
